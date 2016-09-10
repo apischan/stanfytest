77e10d64becb81e1c9366302f06267cf83aefe7a
apischan <andrew.pischanski@gmail.com>
2016-09-11 01:02:44 +0300

    added new guice DI module

--- stanfytest-db/src/main/java/com/apischan/stanfytest/guice/DatabaseModule.java
+++ stanfytest-db/src/main/java/com/apischan/stanfytest/guice/DatabaseModule.java
@@ -1,5 +1,7 @@
 package com.apischan.stanfytest.guice;
 
+import com.apischan.stanfytest.repository.CandidateRepository;
+import com.apischan.stanfytest.repository.CandidateRepositoryImpl;
 import com.apischan.stanfytest.repository.util.ConnectionProvider;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
@@ -8,5 +10,6 @@ public class DatabaseModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(ConnectionProvider.class).in(Scopes.SINGLETON);
+        bind(CandidateRepository.class).to(CandidateRepositoryImpl.class);
     }
 }
