66cfbd31f7204393612afef9f5a3ccab4848aadc
apischan <andrew.pischanski@gmail.com>
2016-09-11 15:48:22 +0300

    added id field to dto

--- stanfytest-db/src/main/java/com/apischan/stanfytest/dto/CandidateDto.java
+++ stanfytest-db/src/main/java/com/apischan/stanfytest/dto/CandidateDto.java
@@ -5,11 +5,11 @@ import lombok.*;
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @RequiredArgsConstructor
 public class CandidateDto {
 
+    private Integer id;
     private @NonNull String firstname;
     private @NonNull String lastname;
     private List<SkillDto> skills;
--- stanfytest-db/src/main/java/com/apischan/stanfytest/dto/SkillDto.java
+++ stanfytest-db/src/main/java/com/apischan/stanfytest/dto/SkillDto.java
@@ -1,12 +1,16 @@
 package com.apischan.stanfytest.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class SkillDto {
 
-    private String skillName;
+    private Integer id;
+    private @NonNull String skillName;
 
 }
