c93313f1b4e1f0980f9f7729a7c0fdf749f590c3
apischan <andrew.pischanski@gmail.com>
2016-09-11 17:07:23 +0300

    removed unneeded methods

--- stanfytest-db/src/main/java/com/apischan/stanfytest/repository/SkillRepository.java
+++ stanfytest-db/src/main/java/com/apischan/stanfytest/repository/SkillRepository.java
@@ -3,19 +3,7 @@ package com.apischan.stanfytest.repository;
 import com.apischan.stanfytest.dto.SkillDto;
 import org.jooq.DSLContext;
 
-import java.util.Optional;
-
-import static com.apischan.stanfytest.jooq.Tables.SKILL;
-
 public interface SkillRepository {
-
-    /**
-     * Get skill instance id by it's name
-     *
-     * @param name skill name
-     * @return optional of skill instance id
-     */
-    Optional<Integer> getSkillIdByName(String name, DSLContext context);
 
     /**
      * Save skill record into database
--- stanfytest-db/src/main/java/com/apischan/stanfytest/repository/impl/SkillRepositoryImpl.java
+++ stanfytest-db/src/main/java/com/apischan/stanfytest/repository/impl/SkillRepositoryImpl.java
@@ -4,20 +4,9 @@ import com.apischan.stanfytest.dto.SkillDto;
 import com.apischan.stanfytest.repository.SkillRepository;
 import org.jooq.DSLContext;
 
-import java.util.Optional;
-
 import static com.apischan.stanfytest.jooq.Tables.SKILL;
 
 public class SkillRepositoryImpl implements SkillRepository {
-
-    @Override
-    public Optional<Integer> getSkillIdByName(String name, DSLContext context) {
-        return context
-                .select(SKILL.ID)
-                .from(SKILL)
-                .where(SKILL.NAME.equalIgnoreCase(name))
-                .fetchOptional(SKILL.ID);
-    }
 
     @Override
     public Integer saveSkill(SkillDto skill, DSLContext context) {
