20ae86558e7d92670adf693022ff0540d8da5d02
apischan <andrew.pischanski@gmail.com>
2016-09-11 15:48:05 +0300

    cleanup

--- stanfytest-db/src/main/java/com/apischan/stanfytest/repository/impl/SkillRepositoryImpl.java
+++ stanfytest-db/src/main/java/com/apischan/stanfytest/repository/impl/SkillRepositoryImpl.java
@@ -2,24 +2,13 @@ package com.apischan.stanfytest.repository.impl;
 
 import com.apischan.stanfytest.dto.SkillDto;
 import com.apischan.stanfytest.repository.SkillRepository;
-import com.apischan.stanfytest.repository.util.JooqConnectionProvider;
-import com.google.inject.Inject;
 import org.jooq.DSLContext;
-import org.jooq.SQLDialect;
 
 import java.util.Optional;
 
 import static com.apischan.stanfytest.jooq.Tables.SKILL;
-import static org.jooq.impl.DSL.using;
 
 public class SkillRepositoryImpl implements SkillRepository {
-
-    private JooqConnectionProvider connectionProvider;
-
-    @Inject
-    public SkillRepositoryImpl(JooqConnectionProvider connectionProvider) {
-        this.connectionProvider = connectionProvider;
-    }
 
     @Override
     public Optional<Integer> getSkillIdByName(String name, DSLContext context) {
