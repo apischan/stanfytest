6e7eabb2404b19e55e97d72a006ed8391e36a8b2
apischan <andrew.pischanski@gmail.com>
2016-09-12 22:35:49 +0300

    added logging of requests and added static import

--- stanfytest-rest/src/main/java/com/apischan/stanfytest/route/RouterChain.java
+++ stanfytest-rest/src/main/java/com/apischan/stanfytest/route/RouterChain.java
@@ -2,22 +2,29 @@ package com.apischan.stanfytest.route;
 
 import com.apischan.stanfytest.handler.CandidateHandlers;
 import com.apischan.stanfytest.handler.SkillHandlers;
+import lombok.extern.slf4j.Slf4j;
 import ratpack.func.Action;
 import ratpack.handling.Chain;
+import ratpack.handling.RequestLogger;
 
+import static com.apischan.stanfytest.handler.CandidateHandlers.*;
+import static com.apischan.stanfytest.handler.SkillHandlers.*;
+
+@Slf4j
 public class RouterChain implements Action<Chain> {
 
     @Override
     public void execute(Chain chain) throws Exception {
         chain
+                .all(RequestLogger.ncsa(log))
                 // candidate endpoints
-                .get("candidates", CandidateHandlers.AllCandidatesHandler.class)
-                .get("candidates/:id", CandidateHandlers.CandidateByIdHandler.class)
-                .post("candidate", CandidateHandlers.StoreCandidateHandler.class)
+                .get("candidates", AllCandidatesHandler.class)
+                .get("candidates/:id", CandidateByIdHandler.class)
+                .post("candidate", StoreCandidateHandler.class)
 
                 // skill endpoints
-                .get("skills", SkillHandlers.SkillByQueryHandler.class)
-                .get("skills/:id", SkillHandlers.SkillByIdHandler.class);
+                .get("skills", SkillByQueryHandler.class)
+                .get("skills/:id", SkillByIdHandler.class);
     }
 
 }
