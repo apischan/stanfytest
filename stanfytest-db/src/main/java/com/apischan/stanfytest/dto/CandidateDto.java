b6f215140757010918ee3c0aa4e55774f8244623
apischan <andrew.pischanski@gmail.com>
2016-09-12 15:12:14 +0300

    added empty list as default value for skills

--- stanfytest-db/src/main/java/com/apischan/stanfytest/dto/CandidateDto.java
+++ stanfytest-db/src/main/java/com/apischan/stanfytest/dto/CandidateDto.java
@@ -2,6 +2,7 @@ package com.apischan.stanfytest.dto;
 
 import lombok.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @Data
@@ -12,6 +13,6 @@ public class CandidateDto {
     private @NonNull Integer id;
     private @NonNull String firstname;
     private @NonNull String lastname;
-    private List<SkillDto> skills;
+    private List<SkillDto> skills = Collections.emptyList();
 
 }
