595c9e4d3b53db278053c7893eef46f7db17257c
apischan <andrew.pischanski@gmail.com>
2016-09-12 22:39:06 +0300

    removed redundant creation of the sequences and added autoincrement for id field

--- stanfytest-it/scripts/schema.sql
+++ stanfytest-it/scripts/schema.sql
@@ -1,22 +1,19 @@
 CREATE TABLE candidate
 (
-  id INTEGER NOT NULL,
+  id INTEGER auto_increment,
   firstname VARCHAR(50),
   lastname VARCHAR(50)
 );
-CREATE SEQUENCE candidate_id_seq START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE candidate_skill
 (
-  id INTEGER NOT NULL,
+  id INTEGER auto_increment,
   candidate_id INTEGER,
   skill_id INTEGER
 );
-CREATE SEQUENCE candidate_skill_id_seq START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE skill
 (
-  id INTEGER NOT NULL,
+  id INTEGER auto_increment,
   name VARCHAR(50)
 );
-CREATE SEQUENCE skill_id_seq START WITH 1 INCREMENT BY 1;
