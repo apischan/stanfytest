e2f091b9628f310772b392657121c2d53d6043b3
apischan <andrew.pischanski@gmail.com>
2016-09-13 07:39:51 +0300

    removed unneeded files

--- stanfytest-db/scripts/vacancy.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE vacancy
-(
-    id SERIAL,
-    company_name VARCHAR(100),
-    title VARCHAR(200),
-    description TEXT
-);
--- stanfytest-db/scripts/vacancy_skill.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE vacancy_skill
-(
-    id SERIAL,
-    vacancy_id INTEGER,
-    skill_id INTEGER
-);
